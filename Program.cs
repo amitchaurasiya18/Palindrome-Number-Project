b29c6141fd1dae9a39af6393fdc277b7dc745953
Amit Dharmandra Chaurasiya <Amit.Chaurasiya@asl.mum.sst>
2024-07-10 17:42:29 +0530

    Files copy pasted from local directory

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber, remainderVariable, sum = 0, copyOfInputNumber;
+            Console.Write("Enter the Number: ");
+            inputNumber = int.Parse(Console.ReadLine());
+            copyOfInputNumber = inputNumber;
+
+            while (inputNumber > 0)
+            {
+                remainderVariable = inputNumber % 10;
+                sum = (sum * 10) + remainderVariable;
+                inputNumber = inputNumber / 10;
+            }
+
+            if (copyOfInputNumber == sum)
+                Console.WriteLine(copyOfInputNumber + " is a Palindrome Number.");
+            else
+                Console.WriteLine(copyOfInputNumber + " is not a Palindrome Number.");
+        }
+    }
+}
